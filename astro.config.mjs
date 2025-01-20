3950eb44dd5ae165b9d9dc99a9959c7e458c0cba
RachelD <rachel11072003@gmail.com>
2025-01-20 18:15:12 -0500

    add: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'static', 
-  base: '/Rachel-Duarte-Portfolio/', 
-
+  base: '/Rachel-Duarte-Portfolio/', // Especifica el nombre de tu repositorio
 });
