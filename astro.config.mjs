41e7a82740e392bfcb9e79504d9c145ad5d30597
RachelD <rachel11072003@gmail.com>
2025-01-20 20:16:45 -0500

    cambio de dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  base: '/Rachel-Duarte-Portfolio/', // Especifica el nombre de tu repositorio
+  base: '/', // Especifica el nombre de tu repositorio
+  site: 'http://rachelduarteportfolio.me',
   outDir: 'dist',
 });
