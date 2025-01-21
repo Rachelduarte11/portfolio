31cd0810cab3acaf646abaad80fc5e37de04e124
Esclender <elesclenderlugo@gmail.com>
2025-01-20 22:55:36 -0500

    update site URL in astro.config.mjs to point to portfolio_rachel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://esclender.github.io',
+  site: 'https://esclender.github.io/portfolio_rachel/',
   outDir: 'dist',
   base: '/',
 });
